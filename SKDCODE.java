4610c420fe279653313396750cef8c949533f9e7
DK703 <91508871+Firecrafter703@users.noreply.github.com>
2022-05-01 16:50:30 -0700

    f

--- SKDCODE.java
+++ SKDCODE.java
@@ -1,4 +1,10 @@
 public class SKDCODE
 {
 
+    public static int subtract(int m, int n)
+    {
+    //m-n
+    return m;
+    }
+
 }
---
+++ SKDTEST.java
@@ -0,0 +1,13 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+
+public class SKDTEST {
+@Test
+public void subtractionTests()
+{
+    assertEquals(2, SKDCODE.subtract(3,1));
+ 
+}
+
+}
